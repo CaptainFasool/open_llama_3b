f3c7b42813dc6ed86df3bc710c48ba589b44a693
Faisal <faisalbeach25@hotmail.com>
2023-07-04 18:27:26 +0100

    Update guanaco_7b.py

--- guanaco_7b.py
+++ guanaco_7b.py
@@ -1,54 +1,53 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
-import os
 
-def generate_text(model_name, prompt):
-    # Set up the model
-    tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-    model = GPT2LMHeadModel.from_pretrained(model_name)
+# Set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Make sure the model uses GPU
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = model.to(device)
+# Parameters
+model_name = 'KBlueLeaf/guanaco-7b-leh-v2'
 
-    # Encode the prompt
-    input_ids = tokenizer.encode(prompt, return_tensors="pt").to(device)
+def generate_text(model, tokenizer, prompt):
+    # Encode the input and send to device
+    input_ids = tokenizer.encode(prompt, return_tensors='pt').to(device)
+    
+    # Generate response
+    output = model.generate(input_ids, 
+                            max_length=1024, 
+                            temperature=0.65, 
+                            top_k=50, 
+                            top_p=0.95)
+    
+    # Decode the response
+    output_text = tokenizer.decode(output[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
 
-    # Generate text
-    output = model.generate(input_ids, max_length=1000, do_sample=True,
-                            top_p=0.92, top_k=30, temperature=0.6,
-                            pad_token_id=tokenizer.eos_token_id)
+    return output_text
 
-    # Decode the output
-    return tokenizer.decode(output[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
+def chatbot(text):
+    # Load the model and tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+    
+    # Open the file in append mode
+    with open(filename, "a+") as f:
+        f.seek(0)  # go to the beginning of the file
+        previous_conversation = f.read().splitlines()  # read previous conversation
+        f.write(text + "\n")  # save user input
 
+        # Generate a response using the model
+        response = generate_text(model, tokenizer, text)
+        f.write(response + "\n")  # save chatbot response to file
+        f.seek(0)  # go to the beginning of the file again
+        updated_conversation = f.read().splitlines()  # read updated conversation
 
-filename = "conversation.txt"
-if not os.path.exists(filename):
-    open(filename, "w").close()  # create file if it doesn't exist
+    return "\n".join(updated_conversation[len(previous_conversation):])
 
 def main():
-    model_name = 'KBlueLeaf/guanaco-7b-leh-v2'
-
     while True:
-        # Open the file in append mode
-        with open(filename, "a+") as f:
-            f.seek(0)  # go to the beginning of the file
-            previous_conversation = f.read().splitlines()  # read previous conversation
+        prompt = input("Please enter a prompt: ")
+        response = chatbot(prompt)
+        print("Generated Text:")
+        print(response)
 
-            prompt = input("Please enter a prompt: ")
-            f.write('User: ' + prompt + "\n")  # save user input
-            print('Generated Text:')
-            response = generate_text(model_name, prompt)
-            f.write('Assistant: ' + response + "\n")  # save chatbot response to file
-            print(response)
-            print('\n')
-
-            f.seek(0)  # go to the beginning of the file again
-            updated_conversation = f.read().splitlines()  # read updated conversation
-
-        print("\n".join(updated_conversation[len(previous_conversation):]))
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
